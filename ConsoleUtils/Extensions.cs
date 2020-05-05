1d80662f5cd8c595b7e72870c6b38feaa1932fb3
collenirwin <collenirwin@gmail.com>
2020-05-05 02:33:03 -0400

    add start

---
+++ ConsoleUtils/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleUtils
+{
+    public class Extensions
+    {
+        internal static Func<string> ReadLine = Console.ReadLine;
+        internal static Action<string> Write = Console.Write;
+
+        public static string Prompt(string message)
+        {
+            Write(message);
+            return ReadLine();
+        }
+
+        public static string PromptOnNewLine(string message)
+        {
+            return Prompt(message + Environment.NewLine);
+        }
+
+        public static string PromptUntil(string message, Predicate<string> predicate)
+        {
+            string response;
+
+            do
+            {
+                response = Prompt(message);
+            } while (!predicate(response));
+
+            return response;
+        }
+    }
+}
